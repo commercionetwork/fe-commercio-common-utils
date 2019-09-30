ea54630b2a3127ebd64b04f8813ba4e823616902
fulvio <fulvio.zanetti@gmail.com>
2019-09-30 11:40:14 +0200

    Added export of USER_ROLE constants

--- src/index.js
+++ src/index.js
@@ -1,9 +1,15 @@
 import messageManager from './messageManager';
 import sessionManager from './sessionManager';
-import { RESPONSES_KEY } from './constants/message'
+import {
+  RESPONSES_KEY
+} from './constants/message';
+import {
+  USER_ROLE
+} from './constants/session';
 
 export {
   RESPONSES_KEY,
+  USER_ROLE,
   messageManager,
   sessionManager
 };
